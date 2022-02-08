2ae3d49d392661c52bbb049e1f3856074da0d69b
RotenKiwi <joshiparth212@gmail.com>
2022-02-08 13:47:41 +0530

    shortest traversal path

---
+++ Shortest Path Algorithm.cpp
@@ -0,0 +1,6 @@
+/*Given a graph and a source vertex in the graph, find the shortest paths from the source to all vertices in the given graph.
+Generate a shortest path tree with the given source as the root. Maintain 2 sets, one which contains vertices visited and the ither which contains the vertices yet to be visited. At every step we find a vertex that is in the other set(not included set) which is at the minimum distance from the source.
+
+Step1: create a set sptSet, that keeps the track of vertices included in the shortest path set. Initially this set is empty.
+Step2: assign distance value to all vertices in the input graph. initialise all the distances as infinity. assign distance value as zero for the source vertex, so that it is picked first.
+Step3: pick a vertex 'u' from the other set with the lowest distance value and which is not in the sptSet and include it in sptSet. Update distance value of all the vertices adjacent to 'u'.*/ 
